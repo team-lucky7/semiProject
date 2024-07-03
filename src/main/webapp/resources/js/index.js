fb9d4a64359b2c5624c59005ff8cb30f7690f489
jw <as12@ruu.kr>
2024-07-03 18:48:34 +0900

    메인페이지 contextPath 추가 자유게시판 틀 추가

--- src/main/webapp/resources/js/index.js
+++ src/main/webapp/resources/js/index.js
@@ -1,14 +1,3 @@
-$(".nav>li").hover(function () {
-    if ($(this).find("div").css("display") == "none") {
-        $(this).find("div").slideDown();
-    } else {
-        $(this).find("div").slideUp();
-    }
-}, function () {
-    $(this).find("div").slideUp();
-})
-
-
 let j = 3;
 const img = $(".main-imgs-wrap>img")
 setInterval(()=> {
