47317ca5fad2a9ab7c8cd85de9c876e539f49233
jw <as12@ruu.kr>
2024-07-06 23:48:43 +0900

    로그인 수정

--- src/main/webapp/resources/js/login.js
+++ src/main/webapp/resources/js/login.js
@@ -1,3 +1,25 @@
+function loginValidate(){
+    const inputId  = document.getElementsByName[0];
+    const inputPw  = document.getElementsByName[0];
+
+    if(inputId.value.trim().length == 0){
+        return printAlert(inputId, "아이디를 입력해주세요.");
+    }
+
+    if(inputPw.value.trim().length == 0){
+        return printAlert(inputPw, "비밀번호를 입력해주세요.");
+    }
+
+    return true;
+}
+
+function printAlert(el, message){
+    el.focus();
+    alert(message);
+
+    return false;
+}
+
 document.getElementsByName("saveId")[0].addEventListener("change", function(){
     
     if(this.checked){
