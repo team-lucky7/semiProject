0ff09c5efd7845e68858d17ab0eb4add553bf5db
jw <as12@ruu.kr>
2024-07-09 19:21:18 +0900

    회원가입 DAO만 미완

--- src/main/java/semiProject/member/controller/SignUpServlet.java
+++ src/main/java/semiProject/member/controller/SignUpServlet.java
@@ -7,6 +7,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import semiProject.member.model.service.MemberService;
+import semiProject.member.model.vo.Member;
 
 @WebServlet("/signUp")
 public class SignUpServlet extends HttpServlet{
@@ -15,6 +19,46 @@ public class SignUpServlet extends HttpServlet{
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String path = "/WEB-INF/views/member/signUp.jsp";
 		req.getRequestDispatcher(path).forward(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String memberId = req.getParameter("memberId");
+	    String memberPw = req.getParameter("memberPw");
+	    String memberName = req.getParameter("memberName");
+	    String memberDOB = req.getParameter("memberDOB");
+	    String memberEmail = req.getParameter("memberEmail");
+	    String memberTel = req.getParameter("memberTel");
+	    
+	    Member mem = new Member();
+	    mem.setMemberId(memberId);
+	    mem.setMemberPw(memberPw);
+	    mem.setMemberName(memberName);
+	    mem.setMemberDOB(memberDOB);
+	    mem.setMemberEmail(memberEmail);
+	    mem.setMemberTel(memberTel);
+	    
+	    try {
+	    	MemberService service = new MemberService();
+	    	
+	    	int result = service.signUp(mem);
+	    	
+    		HttpSession session =req.getSession();
+			
+    		String path = null;
+			if(result>0) {
+				session.setAttribute("message",	"회원 가입 성공!");
+				path = req.getContextPath();
+			} else {
+				session.setAttribute("message", "회원 가입 실패!");
+				path = req.getContextPath() + "/signUp";
+			}
+			
+			resp.sendRedirect(path);
+	    	
+	    }catch(Exception e) {
+	    	e.printStackTrace();
+	    }
 	}
 	
 }
