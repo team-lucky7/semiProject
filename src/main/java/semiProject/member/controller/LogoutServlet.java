72f50fe6c874197a3d399d01578d2d6f7dd10c95
jw <as12@ruu.kr>
2024-07-16 20:58:34 +0900

    로그아웃 수정

--- src/main/java/semiProject/member/controller/LogoutServlet.java
+++ src/main/java/semiProject/member/controller/LogoutServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@WebServlet("/member/logout")
+@WebServlet("/myPage/logout")
 public class LogoutServlet extends HttpServlet{
 	
 	@Override
