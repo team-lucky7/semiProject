b85b3807e75681259ddac67b0dbff45b957bd736
jw <as12@ruu.kr>
2024-07-17 18:10:03 +0900

    암호화

--- src/main/java/semiProject/common/wrapper/EncryptWrapper.java
+++ src/main/java/semiProject/common/wrapper/EncryptWrapper.java
@@ -21,6 +21,8 @@ public class EncryptWrapper extends HttpServletRequestWrapper{
 		
 		switch(name) {
 		case "inputPw" : 
+		case "currentPw" :
+		case "newPw" :
 			value = getSha512( super.getParameter(name) );
 			break;
 		
