a106d280357d16dcd92f7a174d83072f5ae08ba1
jeoneungi <jeg3339@naver.com>
2024-07-23 18:43:41 +0900

    지역

--- src/main/webapp/resources/js/region.js
+++ src/main/webapp/resources/js/region.js
@@ -5,16 +5,19 @@ let IMAGE_WIDTH = 1200;
 const Prev = document.getElementById("left");
 const Next = document.getElementById("right");
 const images = document.querySelector(".main-content");
+const mainAdd = document.getElementsByClassName("main-add");
+let width = 1200 * mainAdd.length;
 
+images.style.width = width;
 
 function next(){
-  if(curPos < 6){
+  if(curPos < mainAdd.length - 1){
     Prev.removeAttribute('disabled');
     positionValue -= IMAGE_WIDTH;
     images.style.transform = `translateX(${positionValue}px)`;
     curPos += 1;
   }
-  if(curPos === 5){
+  if(curPos < 1){
     Next.setAttribute('disabled', 'true');
   }
 }
@@ -39,12 +42,6 @@ function init(){
 
 init();
 
-const btnAdd1 = document.getElementsByClassName("Btn-add");
-for(let i=0; i<btnAdd1.length; i++){
-  btnAdd1[i].addEventListener("click",function(){
-    images.setAttribute("display", "none");
-  })
-}
 
 // 이미지 지도에서 마커가 표시될 위치입니다 
 var markerPosition  = new kakao.maps.LatLng(37.57850, 126.9770);
@@ -55,12 +52,14 @@ var marker = {
     position: markerPosition
 };
 
-var staticMapContainer  = document.getElementById('staticMap'), // 이미지 지도를 표시할 div  
-    staticMapOption = { 
-        center: new kakao.maps.LatLng(37.57850, 126.9770), // 이미지 지도의 중심좌표
-        level: 3, // 이미지 지도의 확대 레벨
-        marker: marker // 이미지 지도에 표시할 마커 
-    };    
-
+for(let i=0; i<mainAdd.length; i++){
+  var staticMapContainer  = document.getElementsByClassName('staticMap')[i], // 이미지 지도를 표시할 div  
+  staticMapOption = { 
+    center: new kakao.maps.LatLng(37.57850, 126.9770), // 이미지 지도의 중심좌표
+    level: 3, // 이미지 지도의 확대 레벨
+    marker: marker // 이미지 지도에 표시할 마커 
+  };    
+    var staticMap = new kakao.maps.StaticMap(staticMapContainer, staticMapOption);
+}
 // 이미지 지도를 생성합니다
-var staticMap = new kakao.maps.StaticMap(staticMapContainer, staticMapOption);
+
