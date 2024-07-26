505499f50c55ee579b7570fe4c971c3e1f33e267
jw <as12@ruu.kr>
2024-07-26 21:04:29 +0900

    최근 검색어 완료

--- src/main/webapp/resources/js/header.js
+++ src/main/webapp/resources/js/header.js
@@ -8,11 +8,65 @@ $(".nav>li").hover(function () {
     $(this).find("div").slideUp();
 })
 
-if($(".recent-search > ul").html() == ""){
-    $(".recent-search > p").css('display', 'block');
-}else{
-    $(".recent-search > p").css('display', 'none');
+
+function showRecent(){
+	const searchRecent = document.getElementById("recent-search-ul");
+	searchRecent.innerText = "";
+	const recentWord = decodeURI(document.cookie).replace("query=", "");
+	if(recentWord != ""){
+		const words = recentWord.split("||");
+		
+		let i = 0;
+		
+		for(let word of words){
+			const li = document.createElement("li");
+			const a = document.createElement("a");
+			a.innerText = word;
+			a.setAttribute("onclick", "searchWord(this)");
+			li.append(a);
+			const span = document.createElement("span");
+			span.innerHTML = "&times;"
+			span.setAttribute("onclick", "deleteRecent(" + i + ")");
+			li.append(span);
+			searchRecent.append(li);
+		}
+	}
+	
+	recentValidate();
 }
+
+showRecent();
+
+function searchWord(el){
+	location.href = encodeURI("/semiProject/search?query=" + el.innerHTML);
+}
+
+function deleteRecent(index){
+	
+	const recentWord = decodeURI(document.cookie).replace("query=", "");
+	let words = recentWord.split("||");
+	
+	words.splice(index, 1);
+	
+	const temp = words.join("||");
+
+	document.cookie = "query=" + temp;
+	
+	showRecent();
+}
+
+function isfind(){
+	
+}
+
+function recentValidate(){
+	if($(".recent-search > ul").html() == ""){
+	    $(".recent-search > p").css('display', 'block');
+	}else{
+	    $(".recent-search > p").css('display', 'none');
+	}
+}
+
 
 const body = document.getElementsByTagName("body")[0];
 const searchIndex = document.getElementsByClassName("search-index")[0];
